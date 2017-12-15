3e3580340ac687dd86d0166cef6e5bafbb34916a
Dimcho Lakov <mdsektor@gmail.com>
2017-12-15 08:23:58 +0000

    Added 04. CSharp, ASP.NET

---
+++ 04. CSharp, ASP.NET/01. CSharp and ASP.NET - Lab/EF-Nov-2017-Example/EF-Nov-2017-Example/BlogDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace EF_Nov_2017_Example
+{
+    class BlogDbContext : DbContext
+    {
+        public virtual DbSet<Article> Articles { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+    }
+
+    class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1), MaxLength(30)]
+        [Index(IsUnique = true)]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public virtual ICollection<Article> Articles { get; set; }
+
+    }
+
+    class Article
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime? DateTime { get; set; }
+        public int? AuthorId { get; set; }
+        public virtual User Author { get; set; }
+    }
+}
---
+++ 04. CSharp, ASP.NET/01. CSharp and ASP.NET - Lab/EF-Nov-2017-Example/EF-Nov-2017-Example/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_Nov_2017_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new BlogDbContext();
+
+            //db.Articles.Add(new Article
+            //{
+            //    Content = "New Article",
+            //    Title = "My first Article",
+            //    DateTime = DateTime.Now,
+            //    Author = new User
+            //    {
+            //        FullName = "Pesho Peshev",
+            //        Username = "pesho123",
+            //        PasswordHash = "kwqdjpoqwjpqwo"
+            //    }
+            //});
+            //
+            //db.Articles.Add(new Article
+            //{
+            //    Content = "Second Article",
+            //    Title = "My Second Article",
+            //    DateTime = DateTime.Now,
+            //    Author = new User
+            //    {
+            //        FullName = "Gosho Goshev",
+            //        Username = "gosho123",
+            //        PasswordHash = "kwdqwdqwdqwqdjpoqwjpqwo"
+            //    }
+            //});
+
+            //db.Articles.Add(new Article
+            //{
+            //    Content = "Some info here",
+            //    Title = "My third article"
+            //});
+
+            //db.SaveChanges();
+
+            foreach (User user in db.Users)
+            {
+                Console.WriteLine($"Username is: {user.Username}");
+                Console.WriteLine($"Full name: {user.FullName}");
+                Console.WriteLine($"Password: {user.PasswordHash}");
+                Console.WriteLine($@"\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            }
+            Console.WriteLine($"{new string(' ', 30)}");
+            foreach (Article article in db.Articles)
+            {
+                Console.WriteLine($"Title: {article.Title}");
+                Console.WriteLine($"Content: {article.Content}");
+                Console.WriteLine($"Date and Time: {article.DateTime}");
+                Console.WriteLine($"The author is: {article.Author?.Username}");
+                Console.WriteLine($"{new string('-', 30)}");
+            }
+
+        }
+    }
+}
